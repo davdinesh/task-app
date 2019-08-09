f2e3ced52297bad38fb9ac1fdad7c0e36543b951
davdinesh <viswam.dinesh@gmail.com>
2019-08-09 13:39:38 +0530

    new model for task search filter

---
+++ src/app/model/task-search-criteria.ts
@@ -0,0 +1,8 @@
+export class TaskSearchCriteria {
+    taskName: string;
+    parentTaskName: string;
+    priorityFrom: number;
+    priorityTo: number;
+    startDate: Date;
+    endDate: Date;
+}
