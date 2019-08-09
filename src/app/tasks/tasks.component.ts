a77249b42914f2458dcf0fb98e5d4f50e9dc51e0
Dinesh <example@abc.com>
2019-08-09 11:13:42 +0530

    Task search feature

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Task } from '../model/task';
 import { MatTableDataSource, MatSort } from '@angular/material';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
@@ -9,16 +10,31 @@ import { MatTableDataSource, MatSort } from '@angular/material';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  
+  sampleData : any[] = [{"id":1,"parentTask":{"id":1,"name":"Design"},"task":"High Level Design","startDate":"2019-02-01","endDate":"2019-02-10","priority":3},{"id":2,"parentTask":{"id":1,"name":"Design"},"task":"Low Level Design","startDate":"2019-02-11","endDate":"2019-02-15","priority":5},{"id":3,"parentTask":{"id":2,"name":"Development"},"task":"Configuration update","startDate":"2019-08-08","endDate":"2019-08-10","priority":8},{"id":4,"parentTask":{"id":3,"name":"Testing"},"task":"Performance tuning","startDate":"2019-08-08","endDate":"2019-08-10","priority":17}];
   displayedColumns: string[] = ['task_name', 'parent_task','priority','start_date','end_date','actions'];
   data: Task[] = [];
   dataSource: any = null
   isLoadingResults = true;
-  constructor(private api: ApiService) { }
+  taskSearchForm: FormGroup;
+  taskName : string = '';
+  parentTaskName : string = '';
+  priorityFrom : number;
+  priorityTo : number;
+  startDate : Date = null;
+  endDate : Date = null;
+  constructor(private api: ApiService,private formBuilder: FormBuilder) { }
 
   @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit() {
+    this.taskSearchForm = this.formBuilder.group({
+      'taskName': null,
+      'parentTaskName': null,
+      'priorityFrom': null,
+      'priorityTo': null,
+      'startDate': null,
+      'endDate' : null
+    });
     this.api.getTasks()
     .subscribe(res => {
       this.data = res;
