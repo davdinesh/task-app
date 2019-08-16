7056e57a74275c3cd4ab9d0c71166494f38db287
Dinesh <example@abc.com>
2019-08-16 11:04:01 +0530

    Fixed menu color based on activeRoute

--- src/app/task-menu/task-menu.component.ts
+++ src/app/task-menu/task-menu.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-task-menu',
   templateUrl: './task-menu.component.html',
   styleUrls: ['./task-menu.component.css']
 })
-export class TaskMenuComponent implements OnInit {
-
-  constructor() { }
+export class TaskMenuComponent implements OnInit { 
+  
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    
   }
 
 }
